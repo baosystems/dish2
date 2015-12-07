50680cf25c78dda5ef7a5364b53c7be6325e215c
Lars Helge Overland <larshelge@gmail.com>
2015-12-07 19:08:00 +0100

    Minor

--- bin/configManager.js
+++ bin/configManager.js
@@ -66,6 +66,35 @@ exports.isArg = function(arg) {
 }
 
 /**
+* Invokes a POST request.
+* @param url the URL to post to.
+* @param file the path to the file with the content to include as request payload.
+* @param contentType the content type for the HTTP request.
+*/
+exports.postFile = function(url, file, contentType) {
+  fs.readFile(file, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    var options = conf.getOptions().post;
+    options.data = data;
+    options.headers = {
+      'Content-Type': contentType
+    };
+
+    urllib.request(url, options).then(function(result) {
+      if (200 == result.status || 201 == result.status) {
+        console.log('Content successfully uploaded: ' + conf.getArgs().dataset);
+      }
+      else {
+        console.log('Content could not be uploaded, HTTP status code: ' + result.status);
+      }
+    });
+  });
+}
+
+/**
 * Initalizes configuration.
 */
 initAndGetConf = function() {
--- bin/post_custom_dataentryform.js
+++ bin/post_custom_dataentryform.js
@@ -11,36 +11,7 @@ const app = {};
 app.postDataEntryForm = function() {
   var url = conf.getConf().api.baseUrl + '/dataSets/' + conf.getArgs().dataset + '/customDataEntryForm';
   var file = conf.getArgs().file;
-  app.postFile(url, file, 'text/html');
-}
-
-/**
-* Invokes a POST request.
-* @param url the URL to post to.
-* @param file the path to the file with the content to include as request payload.
-* @param contentType the content type for the HTTP request.
-*/
-app.postFile = function(url, file, contentType) {
-  fs.readFile(file, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-
-    var options = conf.getOptions().post;
-    options.data = data;
-    options.headers = {
-      'Content-Type': contentType
-    };
-
-    urllib.request(url, options).then(function(result) {
-      if (200 == result.status || 201 == result.status) {
-        console.log('Content successfully uploaded: ' + conf.getArgs().dataset);
-      }
-      else {
-        console.log('Content could not be uploaded, HTTP status code: ' + result.status);
-      }
-    });
-  });
+  conf.postFile(url, file, 'text/html');
 }
 
 /**
