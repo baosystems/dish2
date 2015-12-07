b6e04f1d8ee004432018b8437218fd1955a3acb1
Lars Helge Overland <larshelge@gmail.com>
2015-12-07 19:18:12 +0100

    Minor

--- bin/post_custom_dataentryform.js
+++ bin/post_custom_dataentryform.js
@@ -1,26 +1,14 @@
 #!/usr/bin/env node
 const conf = require('./configManager.js');
-
 const app = {};
 
-/**
-* Uploads data entry form read from a file for a specific data set.
-*/
-app.postDataEntryForm = function() {
-  var url = conf.getConf().api.baseUrl + '/dataSets/' + conf.getArgs().dataset + '/customDataEntryForm';
-  var file = conf.getArgs().file;
-  conf.postFile(url, file, 'text/html');
-}
-
-/**
-* Runs command.
-*/
 app.run = function() {
   if (!conf.isArg('dataset') || !conf.isArg('file')) {
     return console.log('Usage: node post_dataentryform --dataset <datasetuid> --file <custom-form-file>');
   }
 
-  app.postDataEntryForm();
+  var url = conf.getConf().api.baseUrl + '/dataSets/' + conf.getArgs().dataset + '/customDataEntryForm';
+  conf.postFile(url, conf.getArgs().file, 'text/html');
 }
 
 app.run();
