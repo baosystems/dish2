e4c91903ac826c9927e01ac9ec1add191bbb76bd
Lars Helge Overland <larshelge@gmail.com>
2015-12-09 11:47:50 +0100

    Minor

--- bin/run_integrity_checks.js
+++ bin/run_integrity_checks.js
@@ -18,96 +18,96 @@ const fs = require('fs');
 const app = {
   sqlViewsUrl: conf.getConf().api.baseUrl + '/sqlViews.json?query=integrity_&fields=id,name,description&paging=false',
   sqlViewsBaseUrl: conf.getConf().api.baseUrl + '/sqlViews',
-	emailUrl: conf.getConf().api.baseUrl + '/email/notification',
-	resultsFilename: 'output.tmp',
-	results: {
-		failed: [],
-		successful: []
-	},
-	violatedChecks: 0,
-	violatedRows: 0
+  emailUrl: conf.getConf().api.baseUrl + '/email/notification',
+  resultsFilename: 'output.tmp',
+  results: {
+    failed: [],
+    successful: []
+  },
+  violatedChecks: 0,
+  violatedRows: 0
 };
 
 app.sendEmail = function(text) {
-	var email = {
-		subject: 'Integrity check summary: ' + app.violatedChecks + ' failed checks out of ' + app.sqlViews.length + ' on ' + new Date().toString(),
-		text: text
-	};
+  var email = {
+    subject: 'Integrity check summary: ' + app.violatedChecks + ' failed checks out of ' + app.sqlViews.length + ' on ' + new Date().toString(),
+    text: text
+  };
 
-	var postOptions = {
-		auth: conf.getOptions().get.auth,
-		timeout: conf.getOptions().get.timeout,
-		data: JSON.stringify(email),
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		method: 'post'
-	};
+  var postOptions = {
+    auth: conf.getOptions().get.auth,
+    timeout: conf.getOptions().get.timeout,
+    data: JSON.stringify(email),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'post'
+  };
 
-	var response = urlsync.request(app.emailUrl, postOptions);
+  var response = urlsync.request(app.emailUrl, postOptions);
 
-	if (response && 200 == response.status) {
-		console.log('Email sent');
-	}
-	else {
-		console.log('Email failed');
-		console.log(response);
-	}
+  if (response && 200 == response.status) {
+    console.log('Email sent');
+  }
+  else {
+    console.log('Email failed');
+    console.log(response);
+  }
 }
 
 app.run = function() {
-	console.log('Getting integrity checks at: ' + app.sqlViewsUrl);
+  console.log('Getting integrity checks at: ' + app.sqlViewsUrl);
 
-	var sqlViewResp = urlsync.request(app.sqlViewsUrl, conf.getOptions().get);
-	var sqlViewJson = JSON.parse(sqlViewResp.data.toString('utf8'));
-	app.sqlViews = sqlViewJson.sqlViews;
+  var sqlViewResp = urlsync.request(app.sqlViewsUrl, conf.getOptions().get);
+  var sqlViewJson = JSON.parse(sqlViewResp.data.toString('utf8'));
+  app.sqlViews = sqlViewJson.sqlViews;
 
-	console.log('Running integrity checks');
+  console.log('Running integrity checks');
 
-	for (var i=0; i < app.sqlViews.length; i++) {
-		var sqlView = app.sqlViews[i];
-		var url = app.sqlViewsBaseUrl + '/' + sqlView.id + '/data.json';
+  for (var i=0; i < app.sqlViews.length; i++) {
+    var sqlView = app.sqlViews[i];
+    var url = app.sqlViewsBaseUrl + '/' + sqlView.id + '/data.json';
 
-		var response = urlsync.request(url, conf.getOptions().get);
-		var data = response.data;
-		var json = JSON.parse(data.toString('utf8'));
+    var response = urlsync.request(url, conf.getOptions().get);
+    var data = response.data;
+    var json = JSON.parse(data.toString('utf8'));
 
-		var result = {};
-		result.description = sqlView.description;
-		result.url = url;
+    var result = {};
+    result.description = sqlView.description;
+    result.url = url;
 
-		if (json.rows && json.rows.length > 0) {
-			result.status = 'FAILED';
-			result.violations = json.rows.length;
-			app.violatedChecks++;
-			app.violatedRows += json.rows.length;
-			app.results.failed.push(result);
-		}
-		else {
-			result.status = 'SUCCESSFUL';
-			app.results.successful.push(result);
-		}
+    if (json.rows && json.rows.length > 0) {
+      result.status = 'FAILED';
+      result.violations = json.rows.length;
+      app.violatedChecks++;
+      app.violatedRows += json.rows.length;
+      app.results.failed.push(result);
+    }
+    else {
+      result.status = 'SUCCESSFUL';
+      app.results.successful.push(result);
+    }
 
-		console.log('Got results for check: ' + sqlView.name + ', violations: '  + json.rows.length);
-	}
+    console.log('Got results for check: ' + sqlView.name + ', violations: '  + json.rows.length);
+  }
 
-	console.log('Writing integrity check results');
+  console.log('Writing integrity check results');
 
-	var prettyResults = JSON.stringify(app.results, null, 2);
+  var prettyResults = JSON.stringify(app.results, null, 2);
 
-	fs.writeFile(app.resultsFilename, prettyResults, 'utf8');
+  fs.writeFile(app.resultsFilename, prettyResults, 'utf8');
 
-	console.log('Summary');
-	console.log('-------');
-	console.log('Violated checks: ' + app.violatedChecks);
-	console.log('Violated rows: ' + app.violatedRows);
-	console.log('-------');
+  console.log('Summary');
+  console.log('-------');
+  console.log('Violated checks: ' + app.violatedChecks);
+  console.log('Violated rows: ' + app.violatedRows);
+  console.log('-------');
 
-	console.log('Sending email');
+  console.log('Sending email');
 
-	app.sendEmail(prettyResults);
+  app.sendEmail(prettyResults);
 
-	console.log('Integrity checks done');
+  console.log('Integrity checks done');
 }
 
 app.run();
