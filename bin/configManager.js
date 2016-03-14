a7608940ef7250fd321a591974a00ec6a9c74ea0
Lars Helge Overland <larshelge@gmail.com>
2016-03-14 11:47:41 +0100

    Public namespace

--- bin/configManager.js
+++ bin/configManager.js
@@ -1,5 +1,6 @@
 /**
- * Dish configuration manager.
+ * Dish configuration manager. Public functions are exported at the end of the
+ * module.
  */
 const fs = require('fs');
 const urllib = require('urllib');
@@ -7,53 +8,53 @@ const argv = require('yargs').argv;
 const csvtojson = require('csvtojson');
 const prettyjson = require('prettyjson');
 
-/* TODO conf namespace */
-
+/**
+* Main namespace.
+*/
 var cnf = {
-  uidPattern: new RegExp('^[a-zA-Z]{1}[a-zA-Z0-9]{10}$')
+  uidPattern: new RegExp('^[a-zA-Z]{1}[a-zA-Z0-9]{10}$'),
+  config: undefined,
+  configFile: undefined,
+  configLocation: undefined,
+  configFilename: 'dish.json'
 }
-
-var config,
-    configFile,
-    configLocation,
-    configFilename = 'dish.json';
 
 /**
 * Returns the config.
 */
-exports.getConf = function() {
-  if (config) {
-    return config;
+cnf.getConf = function() {
+  if (cnf.config) {
+    return cnf.config;
   }
   else {
-    return initAndGetConf();
+    return cnf.initAndGetConf();
   }
 }
 
 /**
 * Returns a basic authentication string.
 */
-exports.getAuth = function() {
-  return this.getConf().api.username + ':' + this.getConf().api.password;
+cnf.getAuth = function() {
+  return cnf.getConf().api.username + ':' + cnf.getConf().api.password;
 }
 
 /**
 * Returns a JSON suitable for network operations.
 */
-exports.getOptions = function() {
+cnf.getOptions = function() {
   return {
     get: {
-      auth: this.getAuth(),
+      auth: cnf.getAuth(),
       method: 'get',
       timeout: 3600000
     },
     post: {
-      auth: this.getAuth(),
+      auth: cnf.getAuth(),
       method: 'post',
       timeout: 3600000
     },
     delete: {
-      auth: this.getAuth(),
+      auth: cnf.getAuth(),
       method: 'delete',
       timeout: 3600000
     }
@@ -63,21 +64,21 @@ exports.getOptions = function() {
 /**
 * Returns the command line arguments as an object.
 */
-exports.getArgs = function() {
+cnf.getArgs = function() {
   return argv;
 }
 
 /**
 * Indicates if the given argument was provided from the command line.
 */
-exports.isArg = function(arg) {
+cnf.isArg = function(arg) {
   return !!(argv[arg] && argv[arg].length);
 }
 
 /**
 * Indicates whether the given string is a valid UID.
 */
-exports.isUid = function(str) {
+cnf.isUid = function(str) {
   if (!str || !str.length) {
     return false;
   }
@@ -89,11 +90,11 @@ exports.isUid = function(str) {
  * Reads the CSV file and converts the content to JSON.
  * @param doneFn callback to apply with the JSON structure.
  */
-exports.convertCsvToJson = function(doneFn) {
+cnf.convertCsvToJson = function(doneFn) {
     var Converter = require('csvtojson').Converter;
     var converter = new Converter({});
     converter.on('end_parsed', doneFn);
-    fs.createReadStream(this.getArgs()['file']).pipe(converter);
+    fs.createReadStream(cnf.getArgs()['file']).pipe(converter);
 }
 
 /**
@@ -102,8 +103,8 @@ exports.convertCsvToJson = function(doneFn) {
 * @param file the path to the file with the content to include as request payload.
 * @param contentType the content type for the HTTP request.
 */
-exports.postFile = function(url, file, contentType) {
-  var options = this.getOptions().post;
+cnf.postFile = function(url, file, contentType) {
+  var options = cnf.getOptions().post;
   options.headers = {
     'Content-Type': contentType
   };
@@ -131,13 +132,18 @@ exports.postFile = function(url, file, contentType) {
 * @param url the URL to post to.
 * @param json the JSON data structure to use as payload.
 */
-exports.postJson = function(url, json) {
+cnf.postJson = function(url, json) {
 
-  if (this.isArg('payload-file')) {
-    fs.writeFile(this.getArgs()['payload-file'], JSON.stringify(json));
+  var isPayloadFile = !!cnf.isArg('payload-file'),
+    payloadfile = cnf.getArgs()['payload-file'],
+    isOutputFile = !!cnf.isArg('output-file'),
+    outputFile = cnf.getArgs()['output-file'];
+
+  if (isPayloadFile) {
+    fs.writeFile(payloadfile, JSON.stringify(json));
   }
 
-  var options = this.getOptions().post;
+  var options = cnf.getOptions().post;
   options.content = JSON.stringify(json);
   options.headers = {
     'Content-Type': 'application/json'
@@ -148,13 +154,15 @@ exports.postJson = function(url, json) {
 
   urllib.request(url, options, function(err, data, result) {
 
+    var dataStr = data ? data.toString('utf8') : '';
+
     if (200 == result.status || 201 == result.status) {
-      var resp = JSON.parse(data.toString('utf8'));
+      var resp = JSON.parse(dataStr);
 
       console.log('JSON data successfully imported');
 
-      if (this.isArg('output-file')) {
-        var outputFile = this.getArgs()['output-file'],
+      if (isOutputFile) {
+        var outputFile = outputFile,
           output = JSON.stringify(resp, null, 4);
         fs.writeFile(outputFile, output, 'utf8');
         console.log('Output written to: ' + outputFile);
@@ -167,7 +175,7 @@ exports.postJson = function(url, json) {
       console.log('JSON data import failed');
       console.log('HTTP status code: ' + result.status);
       console.log('Error: ' + err);
-      console.log('Response: ' + data.toString('utf8'));
+      console.log('Response: ' + dataStr);
     }
   });
 }
@@ -175,30 +183,43 @@ exports.postJson = function(url, json) {
 /**
 * Initalizes configuration.
 */
-initAndGetConf = function() {
+cnf.initAndGetConf = function() {
   var dhisHome = process.env.DHIS2_HOME,
       osHome = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 
   if (dhisHome) {
-    configLocation = dhisHome + '/' + configFilename;
-    console.log('Using DHIS2_HOME environment variable pointing to: ' + configLocation);
+    cnf.configLocation = dhisHome + '/' + cnf.configFilename;
+    console.log('Using DHIS2_HOME environment variable pointing to: ' + cnf.configLocation);
   }
   else if (osHome) {
-    configLocation = osHome + '/' + configFilename;
-    console.log('Using your home directory which seems to be: ' + configLocation);
+    cnf.configLocation = osHome + '/' + cnf.configFilename;
+    console.log('Using your home directory which seems to be: ' + cnf.configLocation);
   }
   else {
-    configLocation = configFilename;
-    console.log('Falling back to default config location: ' + configLocation);
+    cnf.configLocation = cnf.configFilename;
+    console.log('Falling back to default config location: ' + cnf.configLocation);
   }
 
   try {
-    configFile = fs.readFileSync(configLocation, 'utf8');
+    cnf.configFile = fs.readFileSync(cnf.configLocation, 'utf8');
   }
   catch (ex) {
     throw new Error('Configuration file "dish.json" was not found or could not be parsed');
   }
 
-  config = JSON.parse(configFile);
-  return config;
+  cnf.config = JSON.parse(cnf.configFile);
+  return cnf.config;
 }
+
+/**
+* Public functions.
+*/
+module.exports.getConf = cnf.getConf;
+module.exports.getAuth = cnf.getAuth;
+module.exports.getOptions = cnf.getOptions;
+module.exports.getArgs = cnf.getArgs;
+module.exports.isArg = cnf.isArg;
+module.exports.isUid = cnf.isUid;
+module.exports.convertCsvToJson = cnf.convertCsvToJson;
+module.exports.postFile = cnf.postFile;
+module.exports.postJson = cnf.postJson;
